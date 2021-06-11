21dec87816304984f2ef68f2cb248f004f4aba88
Bilal Saad <bilalsaad2005@gmail.com>
2021-06-11 22:14:01 +0300

    Android App - Teaching Math For Kids

---
+++ app/src/main/assets/www/js/app.js
@@ -0,0 +1,94 @@
+  function play1(){
+       var audio = document.getElementById("audio1");
+       audio.play();
+   }  
+
+   function play2(){
+       var audio = document.getElementById("audio2");
+       audio.play();
+   }  
+
+   function play3(){
+       var audio = document.getElementById("audio3");
+       audio.play();
+   }  
+
+   function play4(){
+       var audio = document.getElementById("audio4");
+       audio.play();
+   }  
+
+   function play5(){
+       var audio = document.getElementById("audio5");
+       audio.play();
+   }  
+
+   function play6(){
+       var audio = document.getElementById("audio6");
+       audio.play();
+   }  
+
+   function play7(){
+       var audio = document.getElementById("audio7");
+       audio.play();
+   }  
+
+   function play8(){
+       var audio = document.getElementById("audio8");
+       audio.play();
+   }  
+
+   function play9(){
+       var audio = document.getElementById("audio9");
+       audio.play();
+   }  
+
+    function play10(){
+       var audio = document.getElementById("audio10");
+       audio.play();
+   }  
+
+   function play20(){
+       var audio = document.getElementById("audio20");
+       audio.play();
+   }  
+
+   function play30(){
+       var audio = document.getElementById("audio30");
+       audio.play();
+   }  
+
+   function play40(){
+       var audio = document.getElementById("audio40");
+       audio.play();
+   }  
+
+   function play50(){
+       var audio = document.getElementById("audio50");
+       audio.play();
+   }  
+
+   function play60(){
+       var audio = document.getElementById("audio60");
+       audio.play();
+   }  
+
+   function play70(){
+       var audio = document.getElementById("audio70");
+       audio.play();
+   }  
+
+   function play80(){
+       var audio = document.getElementById("audio80");
+       audio.play();
+   }  
+
+   function play90(){
+       var audio = document.getElementById("audio90");
+       audio.play();
+   }  
+
+    function play100(){
+       var audio = document.getElementById("audio100");
+       audio.play();
+   }  
